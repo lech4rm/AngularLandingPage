83164db878a1af5db419d84bf589cf9840474114
Leo <le.ch4rm@gmail.com>
2016-07-09 13:27:07 +0530

    second commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+//required
+var gulp = require('gulp'),
+  uglify = require('gulp-uglify');
+
+//tasks
+
+gulp.task('scripts', function(){
+
+});
+
+//default tasks
+
+gulp.task('default' , ['scripts']);
